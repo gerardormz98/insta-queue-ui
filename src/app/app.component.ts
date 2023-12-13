cf428a19b4e7299b742db939d990bb90688e3b52
Gerardo Ramírez <gerardo.rmz98@hotmail.com>
2023-12-12 19:38:03 -0600

    Add loading animation when server is sleeping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { LiveWaitlistService } from './services/live-waitlist.service';
 import { AppAlert, AppNotification } from './model/appNotification';
 import { NotificationService } from './services/notification.service';
 import { Subscription } from 'rxjs';
+import { GuardsCheckEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,32 @@ import { Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   notifications: AppNotification[] = [];
   alert?: AppAlert;
+  loading = false;
+  serverResponded = false;
 
   private appNotified$: Subscription;
   private appAlerted$: Subscription;
 
-  constructor(private liveWaitlistService: LiveWaitlistService, private notificationService: NotificationService) {}
+  constructor(private liveWaitlistService: LiveWaitlistService, private notificationService: NotificationService, private router: Router) {}
 
   ngOnInit(): void {
     this.liveWaitlistService.startConnection();
 
     this.appNotified$ = this.notificationService.appNotified.subscribe((notification) => { this.onNewNotification(notification) });
     this.appAlerted$ = this.notificationService.appAlerted.subscribe((alert) => { this.onNewAlert(alert) });
+
+    setInterval(() => {
+      if (!this.serverResponded) {
+        this.loading = true;
+      }
+    }, 850);
+
+    this.router.events.subscribe(event => {
+      if (event instanceof GuardsCheckEnd) {
+        this.loading = false;
+        this.serverResponded = true;
+      }
+    });
   }
 
   onNewNotification(notification: AppNotification) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './components/notification/notification.component';
 import { AlertComponent } from './components/alert/alert.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationComponent,
-    AlertComponent
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    QRCodeModule
+    QRCodeModule,
+    LoadingSpinnerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
